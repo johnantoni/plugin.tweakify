66092b8b022aa6ccbc6d037654fe221b345f2196
John <john@theworkinggroup.ca>
2009-05-28 21:08:10 -0400

    fix

---
+++ lib/tweakify/patch.rb
@@ -0,0 +1,18 @@
+module Patch
+
+  def patch_uploads(file)
+    # patch for file uploads, upload < 20k are stringio streams
+    # this makes sure uploads are always Tempfile objects
+    if file.kind_of? StringIO
+      file.rewind
+      realFile = Tempfile.new(file.original_filename)
+      while not file.eof?
+        realFile.write file.read
+      end
+    else
+      realFile = file
+    end
+    return realFile
+  end
+
+end
