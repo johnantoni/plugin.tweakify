6c0a11a032e721805759e5c5c385dd81a9878091
John <john@theworkinggroup.ca>
2009-06-11 13:26:55 +0100

    fix

--- lib/helpers/cache.rb
+++ lib/helpers/cache.rb
@@ -1,11 +1,13 @@
-module Cache
+module Tweakify
+  module Cache
 
-  def empty_html_cache
-    cache_dir = ActionController::Base.page_cache_directory
-    unless cache_dir == RAILS_ROOT+"/public"
-      FileUtils.rm_r(Dir.glob(cache_dir+"/*")) rescue Errno::ENOENT
-      RAILS_DEFAULT_LOGGER.info("Cache directory '#{cache_dir}' fully sweeped.")
+    def empty_html_cache
+      cache_dir = ActionController::Base.page_cache_directory
+      unless cache_dir == RAILS_ROOT+"/public"
+        FileUtils.rm_r(Dir.glob(cache_dir+"/*")) rescue Errno::ENOENT
+        RAILS_DEFAULT_LOGGER.info("Cache directory '#{cache_dir}' fully sweeped.")
+      end
     end
-  end
 
+  end
 end
